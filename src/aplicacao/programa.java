aa8849cd68928ec198397bf0298adb20deee3faa
NandoMartins07 <nando.costa_martins@yahoo.com.br>
2021-01-03 21:42:19 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,13 +1,13 @@
 package aplicacao;
 
-import tabuleiro.Posição;
-import tabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class programa {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		IU.printTabuleiro(partidaXadrez.getPeças());
 		
 
 	}
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,5 +1,40 @@
 package tabuleiro;
 
 public class Tabuleiro {
+	private int linhas;
+	private int colunas;
+	private Peça[][] peças;
+	
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		peças = new Peça[linhas][colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	public Peça peça(int linha, int coluna) {
+		return peças[linha][coluna];
+	}
+	
+	public Peça peça(Posição posição) {
+		return peças[posição.getLinha()][posição.getColuna()];
+	}
+	
+	
 
 }
