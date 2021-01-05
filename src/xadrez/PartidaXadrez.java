8b4ef46e6213362f2669f19d0f50cabf6db369c6
NandoMartins07 <nando.costa_martins@yahoo.com.br>
2021-01-05 19:35:13 -0300

    Melhorei a visualização do tabuleiro no terminal do git bash

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -28,9 +28,20 @@ public class PartidaXadrez {
 	}
 	
 	private void iniciarPartida() {
-		colocarNovaPeça('b', 6, new Torre(tabuleiro, Cor.Branca));
-		colocarNovaPeça('e', 8, new Rei(tabuleiro, Cor.Preta));
-		colocarNovaPeça('e', 1, new Rei(tabuleiro, Cor.Branca));
+		
+		colocarNovaPeça('c', 1, new Torre(tabuleiro, Cor.Branca));
+		colocarNovaPeça('c', 2, new Torre(tabuleiro, Cor.Branca));
+		colocarNovaPeça('d', 2, new Torre(tabuleiro, Cor.Branca));
+		colocarNovaPeça('e', 2, new Torre(tabuleiro, Cor.Branca));
+		colocarNovaPeça('e', 1, new Torre(tabuleiro, Cor.Branca));
+		colocarNovaPeça('d', 1, new Rei(tabuleiro, Cor.Branca));
+
+		colocarNovaPeça('c', 7, new Torre(tabuleiro, Cor.Preta));
+		colocarNovaPeça('c', 8, new Torre(tabuleiro, Cor.Preta));
+		colocarNovaPeça('d', 7, new Torre(tabuleiro, Cor.Preta));
+		colocarNovaPeça('e', 7, new Torre(tabuleiro, Cor.Preta));
+		colocarNovaPeça('e', 8, new Torre(tabuleiro, Cor.Preta));
+		colocarNovaPeça('d', 8, new Rei(tabuleiro, Cor.Preta));
 	}
 
 }
